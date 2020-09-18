61baa7bd801fe4feebb7828da47c834b47e279f9
Tiana Husted <tiana.husted@gmail.com>
2020-09-17 22:55:43 -0700

    Adding code to install and register the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
-console.log("Hi from your service-worker.js file!");
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/styles.css',
+  '/index.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+//Installing and registering service worker
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached successfully!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
